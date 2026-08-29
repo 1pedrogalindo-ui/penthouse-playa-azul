0268b64ab9a897e6c0acf5e58721292aed6c08dc
1pedrogalindo-ui <1pedrogalindo@gmail.com>
2026-08-29 10:39:57 -0500

    Publish optimized Next.js export for GitHub Pages

---
+++ _next/static/N92hVtYUCF3Qfy5cj5SSp/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/penthouse-playa-azul//_next/:path+",
+        "destination": "/penthouse-playa-azul/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
